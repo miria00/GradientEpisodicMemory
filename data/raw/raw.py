b3db50d17226c0bd8215c3856d122deef4e2fa20
Christopher Wolff <cw0@stanford.edu>
2021-11-10 21:34:44 -0800

    Format part 2

--- data/raw/raw.py
+++ data/raw/raw.py
@@ -15,7 +15,9 @@ mnist_path = "mnist.npz"
 
 # URL from: https://www.cs.toronto.edu/~kriz/cifar.html
 if not os.path.exists(cifar_path):
-    subprocess.call("wget https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz", shell=True)
+    subprocess.call(
+        "wget https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz", shell=True
+    )
 
 subprocess.call("tar xzfv cifar-100-python.tar.gz", shell=True)
 
@@ -23,27 +25,29 @@ subprocess.call("tar xzfv cifar-100-python.tar.gz", shell=True)
 if not os.path.exists(mnist_path):
     subprocess.call("wget https://s3.amazonaws.com/img-datasets/mnist.npz", shell=True)
 
+
 def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
+    with open(file, "rb") as fo:
+        dict = pickle.load(fo, encoding="bytes")
     return dict
 
-cifar100_train = unpickle('cifar-100-python/train')
-cifar100_test = unpickle('cifar-100-python/test')
 
-x_tr = torch.from_numpy(cifar100_train[b'data'])
-y_tr = torch.LongTensor(cifar100_train[b'fine_labels'])
-x_te = torch.from_numpy(cifar100_test[b'data'])
-y_te = torch.LongTensor(cifar100_test[b'fine_labels'])
+cifar100_train = unpickle("cifar-100-python/train")
+cifar100_test = unpickle("cifar-100-python/test")
 
-torch.save((x_tr, y_tr, x_te, y_te), 'cifar100.pt')
+x_tr = torch.from_numpy(cifar100_train[b"data"])
+y_tr = torch.LongTensor(cifar100_train[b"fine_labels"])
+x_te = torch.from_numpy(cifar100_test[b"data"])
+y_te = torch.LongTensor(cifar100_test[b"fine_labels"])
 
-f = np.load('mnist.npz')
-x_tr = torch.from_numpy(f['x_train'])
-y_tr = torch.from_numpy(f['y_train']).long()
-x_te = torch.from_numpy(f['x_test'])
-y_te = torch.from_numpy(f['y_test']).long()
+torch.save((x_tr, y_tr, x_te, y_te), "cifar100.pt")
+
+f = np.load("mnist.npz")
+x_tr = torch.from_numpy(f["x_train"])
+y_tr = torch.from_numpy(f["y_train"]).long()
+x_te = torch.from_numpy(f["x_test"])
+y_te = torch.from_numpy(f["y_test"]).long()
 f.close()
 
-torch.save((x_tr, y_tr), 'mnist_train.pt')
-torch.save((x_te, y_te), 'mnist_test.pt')
+torch.save((x_tr, y_tr), "mnist_train.pt")
+torch.save((x_te, y_te), "mnist_test.pt")
