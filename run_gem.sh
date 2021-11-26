35f6b1e5ccd0a0e58203cb443b4e5731b797f4c5
Christopher Wolff <cw0@stanford.edu>
2021-11-25 20:51:52 -0800

    asdf

--- run_gem.sh
+++ run_gem.sh
@@ -5,10 +5,10 @@ MNIST_ROTA="--n_layers 2 --n_hiddens 100 --data_path data/ --save_path results/ 
 MNIST_PERM="--n_layers 2 --n_hiddens 100 --data_path data/ --save_path results/ --batch_size 10 --log_every 100 --samples_per_task 1000 --data_file mnist_permutations.pt --cuda no  --seed 0"
 CIFAR_100i="--n_layers 2 --n_hiddens 100 --data_path data/ --save_path results/ --batch_size 10 --log_every 100 --samples_per_task 2500 --data_file cifar100.pt           --cuda yes --seed 0"
 
-# model "GEM"
-$MY_PYTHON main.py $MNIST_ROTA --model gem --lr 0.01 --n_memories 256 --memory_strength 0.5
-#$MY_PYTHON main.py $MNIST_PERM --model gem --lr 0.1 --n_memories 256 --memory_strength 0.5
-#$MY_PYTHON main.py $CIFAR_100i --model gem --lr 0.1 --n_memories 256 --memory_strength 0.5
+# Original GEM
+# $MY_PYTHON main.py $MNIST_ROTA --model gem_original --lr 0.01 --n_memories 256 --memory_strength 0.5
+$MY_PYTHON main.py $MNIST_PERM --model gem_original --lr 0.1 --n_memories 256 --memory_strength 0.5
+# $MY_PYTHON main.py $CIFAR_100i --model gem_original --lr 0.1 --n_memories 256 --memory_strength 0.5
 
 # plot results
 cd results/
